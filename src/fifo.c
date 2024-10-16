cca03f295dc1dfcb38f56fb203504dcaf9a8859d
EFranco <eddfranco87@gmail.com>
2024-10-16 17:36:59 -0600

    apdtated signal fifo

--- src/fifo.c
+++ src/fifo.c
@@ -8,5 +8,10 @@
 
 void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id)
 {
-   
+    struct request_msg data; 
+   xQueueReceive(requests, &data, portMAX_DELAY);
+   data.output = data.input +5;
+   data.handled_by = id;
+   vTaskDelay(10);
+    xQueueSendToBack(results, &data, portMAX_DELAY);
 }
